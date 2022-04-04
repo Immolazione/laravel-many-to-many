d92e87b55f8466d22aadd58d928499d2cb5c4a44
Immolazione <federico.deered@gmail.com>
2022-04-04 21:06:27 +0200

    minor changes

--- app/Models/post.php
+++ app/Models/post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class post extends Model
+class Post extends Model
 {
     protected $fillable = ['title', 'content', 'slug', 'category_id', 'created_at', 'updated_at'];
 
